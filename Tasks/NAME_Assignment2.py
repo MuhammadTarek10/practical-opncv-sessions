d9acc0b05065bf9724b534d2d021ba18955f1155
MuhammadTarek10 <mohammadtarek000@gmail.com>
2021-09-21 19:16:08 +0200

    task 2 added

--- Tasks/NAME_Assignment2.py
+++ Tasks/NAME_Assignment2.py
@@ -3,10 +3,12 @@ import numpy as np
 
 def segment():
     """
-        inputs:
-            None
-        outputs:
-            show a camera feed with specific color filters
+        Required:
+            make 2 modes, segmentation mode and normal mode
+            normal mode is the normal camera without any changes
+            segment mode is making a mask with specific colors (red, green, orange, blue)
+            when the user click on a key from ['o', 'g', 'b', 'r'] the video feed itself
+            is switched to only show that specific color on the video
     """
     
     cap = cv2.VideoCapture(0)
